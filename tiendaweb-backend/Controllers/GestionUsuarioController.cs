f0580b025f6abc8397a5dd575145b6e39f683f6d
alainflores-T <alain.flores@ucb.edu.bo>
2026-05-28 16:14:20 -0400

    ACTUALIZACION DE USUARIOS

---
+++ tiendaweb-backend/Controllers/GestionUsuarioController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tiendaweb_backend.Datos;
+using tiendaweb_backend.Negocio;
+
+
+namespace tiendaweb_backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GestionUsuarioController :  ControllerBase
+{
+    private GestionUsuario _gestionuser;
+
+    public GestionUsuarioController()
+    {
+        _gestionuser = new GestionUsuario();
+    }
+
+    [HttpGet]
+    public IEnumerable<Usuario> Listarusuario()
+    {
+        return Enumerable.Range(0, _gestionuser.listasdeusaurios.Count).Select(index => new Usuario
+        {
+            Id = _gestionuser.listasdeusaurios[index].Id,
+            nombre = _gestionuser.listasdeusaurios[index].nombre
+
+        });
+    }
+
+    [HttpPost]
+    public void CrearUsuario(Usuario usuario)
+    {
+        _gestionuser.CrearUsuario(usuario);
+    }
+
+    [HttpGet]
+    public Usuario? ObtenerUsuario(string email , string password)
+    {
+        return _gestionuser.encontrarUsuario(email, password);
+    }
+        
+    
+}
---
+++ tiendaweb-backend/Negocio/GestionUsuario.cs
@@ -0,0 +1,71 @@
+﻿using tiendaweb_backend.Datos;
+
+namespace tiendaweb_backend.Negocio;
+
+public class GestionUsuario
+{
+    public List<Usuario> listasdeusaurios;
+
+    public GestionUsuario()
+    {
+        // base de datos para poder ver los usuarios.
+        listasdeusaurios = new List<Usuario>
+        {
+            new Usuario {nombre = "Alain" , email = "prueba",contrasena = "prueba"},
+            new Usuario{ nombre = "Christian" , email = "prueba2" , contrasena = "prueba2"}
+        };
+    }
+    
+    public Usuario encontrarUsuario(string email, string contrasena)
+    {
+        Usuario usuario = null;
+        foreach (var VARIABLE in listasdeusaurios)
+        {
+            if (VARIABLE.email.Equals(email) && VARIABLE.contrasena.Equals(contrasena))
+            {
+                 usuario = VARIABLE;
+            }
+        }
+
+        return usuario;
+    }
+
+    public void CrearUsuario(Usuario usuario)
+    {
+        if (usuario == null) return;
+        foreach (var VARIABLE in listasdeusaurios)
+        {
+            if (VARIABLE.email.Equals(usuario.email) && VARIABLE.contrasena.Equals(usuario.contrasena))
+            {
+                return;
+            }
+        }
+
+        usuario.Id = listasdeusaurios.Count + 1;
+        
+        listasdeusaurios.Add(usuario);
+        
+    }
+
+    public bool verificardatospersonales(string email, string contrasena)
+    {
+        foreach (var VARIABLE in listasdeusaurios)
+        {
+            if (VARIABLE.email.Equals(email) && VARIABLE.contrasena.Equals(contrasena))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    
+    
+    
+    
+    
+    
+   
+    
+}
