c5929806921b34c41044b05aedd1e527bcdaeebb
alainflores-T <alain.flores@ucb.edu.bo>
2026-05-29 18:33:13 -0400

    alain

--- tiendaweb-backend/Datos/Producto.cs
+++
@@ -1,9 +0,0 @@
-namespace tiendaweb_backend.Datos;
-
-public class Producto
-{
-    public int Id { get; set; }
-    public string? Nombre { get; set; }
-    public string? Descripcion { get; set; }
-    public double? Precio { get; set; }
-}
