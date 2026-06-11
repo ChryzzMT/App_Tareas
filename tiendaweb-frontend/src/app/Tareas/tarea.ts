e42a52cb55947886e1a99feb30fe6fbb096b332c
Christian <chryztianmt@gmail.com>
2026-06-11 15:52:11 -0400

    cmabioas

--- tiendaweb-frontend/src/app/Tareas/tarea.ts
+++ tiendaweb-frontend/src/app/Tareas/tarea.ts
@@ -3,33 +3,36 @@ import { ApiClient } from '../core/http/api-client';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Materia } from '../Materias/materia'
+import { Materia } from '../Materias/materia';
+import {Tarea} from './tareas';
+import {MateriaEdicion} from '../Materias/materia-edicion';
+import {TYPED_NULL_EXPR} from '@angular/compiler';
 
 @Component({
   selector: 'app-tarea',
   imports: [RouterLink, FormsModule],
   templateUrl: './tarea.html'
 })
-export class Tarea {
+export class TareaEdicion {
   private api = inject(ApiClient);
   private router = inject(Router);
   private url = 'http://localhost:5056/GestionTareas';
 
-  tarea = {
-    idTarea: 0,
-    titulo: '',
-    descripcion: '',
-    pesoTarea: 0,
-    fechaEntrega: '',
-    estado: '',
-    idMateria: 0
-
-  };
+  tarea:Tarea={
+    idTarea:0,
+    estado:'',
+    titulo:'',
+    pesoTarea:0,
+    descripcion:'',
+    fechaEntrega:'',
+    idMateria:0,
+    materia:TYPED_NULL_EXPR
+  }
 
   guardar() {
     this.api.post(this.url + '/Crear-Tarea', this.tarea).subscribe({
       next: () => {
-        this.tarea = {idTarea: 0, titulo: '', descripcion: '', fechaEntrega: '', pesoTarea: 0, estado: '', idMateria: 0};
+        // this.tarea = {idTarea: 0, titulo: '', descripcion: '', fechaEntrega: '', pesoTarea: 0, estado: '', idMateria: 0};
         this.router.navigate(['/tareas']);
       },
       error: error => console.error('Error al guardar la tarea', error)
@@ -38,10 +41,7 @@ export class Tarea {
 
   actualizar() {
     this.api.put(this.url + '/Actualizar-Todo', this.tarea).subscribe({
-      next: () => {
-        this.tarea = {idTarea: 0, titulo: '', descripcion: '', fechaEntrega: '', pesoTarea: 0, estado: '', idMateria: 0};
-        this.router.navigate(['/tareas']);
-      },
+      next: () => this.router.navigate(['/tareas']),
       error: error => console.error('Error al actualizar la tarea', error)
     });
   }
@@ -52,8 +52,12 @@ export class Tarea {
 
   ngOnInit() {
     const usuarioId = localStorage.getItem('usuarioId');
+    const state = history.state;
+    if (state && state['tarea']) {
+      this.tarea = state['tarea'];
+      this.esEdicion = true;
+    }
 
-    // Cargar materias del usuario
     this.api.put('http://localhost:5056/GestionMaterias/SetUsuario?userid=' + usuarioId, {}).subscribe({
       next: () => {
         this.api.get<{idMateria: number, nombreMateria: string}[]>('http://localhost:5056/GestionMaterias/lista-materias').subscribe({
